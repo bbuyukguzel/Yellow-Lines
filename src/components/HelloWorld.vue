2b5ed3dfcb912139085e174725fd6ba791cb2ad5
Batuhan Büyükgüzel <bbuyukguzel@gmail.com>
2019-02-12 20:23:02 +0300

    Vuetify added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <div class="hello">
+      <v-app id="inspire">
+        <v-stepper v-model="stepper">
+
+          <v-stepper-header>
+            <div class="step" v-for="(step, index) in steps" :key=index>
+              <v-stepper-step
+                      :edit-icon="'check'"
+                      :complete-icon="'edit'"
+                      :step="index + 1"
+                      :complete="(index + 1 ) <= stepper"
+                      :editable="(index + 1) < stepper">{{ step.label }}
+              </v-stepper-step>
+              <v-divider></v-divider>
+            </div>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+              <v-btn color="primary" @click.native="stepper = 2">Continue</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+              <v-btn flat @click.native="stepper = 1">Previous</v-btn>
+              <v-btn color="primary" @click.native="stepper = 3">Continue</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+              <v-btn flat @click.native="stepper = 2">Previous</v-btn>
+              <v-btn color="primary" @click.prevent="submit">Finish</v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-app>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+    data() {
+        return {
+            stepper: 0,
+            steps: [
+                {
+                    label: 'Mirror Creation',
+                    completed: false,
+                },
+                {
+                    label: 'Selection',
+                    completed: false,
+                },
+                {
+                    label: 'Details',
+                    completed: false,
+                },
+            ],
+        }
   }
 }
 </script>

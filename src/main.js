4dc8b8081b35220d4610fd9fd0f2ef6b60e9b4f0
Batuhan Büyükgüzel <bbuyukguzel@gmail.com>
2019-02-12 20:38:34 +0300

    Vuelidate added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuetify)
+Vue.use(Vuetify, Vuelidate)
 
 new Vue({
   render: h => h(App),

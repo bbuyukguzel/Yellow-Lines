8efc066474f8b041a25252965b2ce08d70803d94
Batuhan Büyükgüzel <bbuyukguzel@gmail.com>
2019-02-27 23:05:30 +0300

    Json data, generated by puppeteer, is returning to web app as response

--- temp-layer/app.py
+++ temp-layer/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import time
 import json
+from flask import jsonify
 import subprocess
 from tempfile import NamedTemporaryFile
 from random import randint
@@ -15,7 +16,7 @@ puppeteerDIR = '../puppeteer-jobs/generate-mirror.js'
 
 @app.route('/generate-mirror', methods=['GET', 'POST'])
 def generate_mirror():
-    time.sleep(randint(0, 3))   # for chaos
+    #time.sleep(randint(0, 3))   # for chaos
     data = request.data
     dataDict = json.loads(data)
 
@@ -23,9 +24,21 @@ def generate_mirror():
         fname = tf.name + '.png'
         img = 'http://localhost:5000/static/mirrors/' + fname.rsplit('\\')[-1]
     print(fname, img, dataDict['url'])
+
     command = 'node {} {} {}'.format(puppeteerDIR, dataDict['url'], fname)
-    subprocess.call(command, shell=True)
-    return 'Don\'t Panic. '+img
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = proc.communicate()
+    p_status = proc.wait()
+
+    # puppeteer job exited without error
+    ret = {}
+    if not err:
+        jfname = fname.rsplit('.')[0] + '.json'
+        with open(jfname) as f:
+            data = json.load(f)
+            ret[img] = data
+
+    return jsonify(ret)
 
 
 if __name__ == '__main__':

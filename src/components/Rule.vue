ed682c1f4da4c8bea0c0f2bec5761d5b9b4fbd63
Batuhan Büyükgüzel <bbuyukguzel@gmail.com>
2019-03-20 20:34:24 +0300

    All external dependencies of query builder removed
    Style is still broken though

--- src/components/Rule.vue
+++ src/components/Rule.vue
@@ -1,96 +1,96 @@
 <template>
-  <div class="form-group and-or-rule col-xs-12">
+    <v-layout and-or-rule row wrap>
+        <v-flex xs3>
+            <v-select v-model="key" :items="options.keys" item-text="name" value="id" label="FIX HERE"></v-select>
+        </v-flex>
 
-    <div class="col-xs-3">
-      <v-select v-model="key" :items="options.keys" item-text="name" value="id" label="FIX HERE"></v-select>
-    </div>
+        <v-flex xs3>
+            <v-select v-model="operator" :items="options.operators" item-text="name" value="id"
+                      label="FIX HERE"></v-select>
+        </v-flex>
 
-    <div class="col-xs-3">
-        <v-select v-model="operator" :items="options.operators" item-text="name" value="id" label="FIX HERE"></v-select>
-    </div>
+        <v-flex xs3>
+            <v-text-field label="Value" v-model="value"></v-text-field>
+        </v-flex>
 
-    <div class="col-xs-3">
-      <v-text-field label="Value" v-model="value"></v-text-field>
-    </div>
-
-    <v-btn fab small dark color="deep-purple lighten-1" @click.prevent="deleteSelf()">
-      <v-icon dark>remove</v-icon>
-    </v-btn>
-  </div>
+        <v-btn fab small dark color="deep-purple lighten-1" @click.prevent="deleteSelf()">
+            <v-icon dark>remove</v-icon>
+        </v-btn>
+    </v-layout>
 </template>
 
 <script>
-  export default {
-    name: 'rule',
-    props: ['options'],
-    watch: {
-      'options.keys.options' () {
-        this.key = -99;
-      },
-      'options.conditions.options' () {
-        this.condition = -99;
-      }
-    },
-    data () {
-      return {
-        key: -99,
-        operator: -99,
-        value: ''
-      }
-    },
-    methods: {
-      deleteSelf () {
-        this.$emit('delete-rule');
-      },
+    export default {
+        name: 'rule',
+        props: ['options'],
+        watch: {
+            'options.keys.options'() {
+                this.key = -99;
+            },
+            'options.conditions.options'() {
+                this.condition = -99;
+            }
+        },
+        data() {
+            return {
+                key: -99,
+                operator: -99,
+                value: ''
+            }
+        },
+        methods: {
+            deleteSelf() {
+                this.$emit('delete-rule');
+            },
 
-      queryFormStatus () {
-        return {
-          'key': this.key,
-          'operator': this.operator,
-          'value': this.value
+            queryFormStatus() {
+                return {
+                    'key': this.key,
+                    'operator': this.operator,
+                    'value': this.value
+                }
+            },
+
+            fillRuleStatus(data) {
+                this.key = data.key;
+                this.operator = data.operator;
+                this.value = data.value;
+            }
         }
-      },
-
-      fillRuleStatus (data) {
-        this.key = data.key;
-        this.operator = data.operator;
-        this.value = data.value;
-      }
     }
-  }
 </script>
 
 <style>
-  .and-or-rule {
-    position: relative;
-    height: 30px;
-    margin-left: 60px !important;
-    margin-bottom: 30px !important;
-    padding-left: 0;
-  }
+    .and-or-rule {
+        position: relative;
+        height: 30px;
+        margin-left: 60px !important;
+        margin-bottom: 30px !important;
+        padding-left: 0;
+    }
 
-  .and-or-rule:before,
-  .and-or-rule:after {
-    content: '';
-    position: absolute;
-    left: -1px;
-    width: 16px;
-    height: calc(50% + 24px);
-    border-color: #c0c5e2;
-    border-style: solid;
-  }
+    .and-or-rule:before,
+    .and-or-rule:after {
+        content: '';
+        position: absolute;
+        left: -1px;
+        width: 16px;
+        height: calc(50% + 24px);
+        border-color: #c0c5e2;
+        border-style: solid;
+    }
 
-  .and-or-rule:before {
-    top: -15px;
-    border-width: 0 0 2px 2px;
-  }
+    .and-or-rule:before {
+        top: -15px;
+        border-width: 0 0 2px 2px;
+    }
 
-  .and-or-rule:after {
-    top: 50%;
-    border-width: 0 0 0 2px;
-  }
+    .and-or-rule:after {
+        top: 50%;
+        border-width: 0 0 0 2px;
+    }
 
-  .and-or-rule:last-child:after {
-    border: none;
-  }
+    .and-or-rule:last-child:after {
+        border: none;
+    }
 </style>
